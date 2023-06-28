17a6f29a8d4f413a1099f09998272a543452e38d
Matthew Bannock <69541270+MattBann@users.noreply.github.com>
2023-06-28 13:53:33 +0100

    Change data structure to be more efficient

--- scripts/resources/graph_edge_data.gd
+++ scripts/resources/graph_edge_data.gd
@@ -2,13 +2,28 @@ class_name GraphEdgeData
 extends Resource
 
 
-@export var from : int
-@export var to : int
-@export var weight : float
+@export var from : int :
+	set (new_from): data["from"] = new_from
+	get: return data.get("from", -1)
+@export var to : int :
+	set (new_to): data["to"] = new_to
+	get: return data.get("to", -1)
+@export var weight : float :
+	set (new_weight): data["weight"] = new_weight
+	get: return data.get("weight", 1)
+
+
+# The actual data. Properties above provide easy access to this dictionary
+@export var data := {
+	from = -1,
+	to = -1,
+	weight = 1,
+}
+
 
 
 # Initialise the edge
-func _init(from_id : int, to_id : int, new_weight : float) -> void:
+func _init(from_id := -1, to_id := -1, new_weight := -1.0) -> void:
 	from = from_id
 	to = to_id
 	weight = new_weight
--- scripts/resources/setup_data.gd
+++ scripts/resources/setup_data.gd
@@ -23,76 +23,80 @@ enum OptionType {
 
 
 # The setup data as a list of dictionaries
-@export var data := [
-	{
-		name = "algorithm",
-		option_type = OptionType.OPTION_LIST,
-		value_type = Algorithm,
-		value = Algorithm.BREADTH_FIRST_SEARCH,
-	},
-	{
-		name = "start_colour",
-		option_type = OptionType.COLOUR_PICK,
-		value_type = TYPE_COLOR,
-		value = Color.GREEN,
-	},
-	{
-		name = "goal_colour",
-		option_type = OptionType.COLOUR_PICK,
-		value_type = TYPE_COLOR,
-		value = Color.ORANGE,
-	},
-	{
-		name = "node_colour",
-		option_type = OptionType.COLOUR_PICK,
-		value_type = TYPE_COLOR,
-		value = Color.LIGHT_SKY_BLUE,
-	},
-	{
-		name = "fill_nodes",
-		option_type = OptionType.SWITCH,
-		value_type = TYPE_BOOL,
-		value = true,
-	},
-	{
-		name = "enable_edge_weights",
-		option_type = OptionType.SWITCH,
-		value_type = TYPE_BOOL,
-		value = false,
-	},
-	{
-		name = "show_heuristics",
-		option_type = OptionType.SWITCH,
-		value_type = TYPE_BOOL,
-		value = false,
-	},
-	{
-		name = "distance_scale",
-		option_type = OptionType.SPIN,
-		value_type = {
-			min = 1,
-			max = 100,
-			step = 1
+@export var data := {
+	options = [
+		{
+			name = "algorithm",
+			option_type = OptionType.OPTION_LIST,
+			value_type = Algorithm,
+			value = Algorithm.BREADTH_FIRST_SEARCH,
 		},
-		value = 10,
+		{
+			name = "start_colour",
+			option_type = OptionType.COLOUR_PICK,
+			value_type = TYPE_COLOR,
+			value = Color.GREEN,
+		},
+		{
+			name = "goal_colour",
+			option_type = OptionType.COLOUR_PICK,
+			value_type = TYPE_COLOR,
+			value = Color.ORANGE,
+		},
+		{
+			name = "node_colour",
+			option_type = OptionType.COLOUR_PICK,
+			value_type = TYPE_COLOR,
+			value = Color.LIGHT_SKY_BLUE,
+		},
+		{
+			name = "fill_nodes",
+			option_type = OptionType.SWITCH,
+			value_type = TYPE_BOOL,
+			value = true,
+		},
+		{
+			name = "enable_edge_weights",
+			option_type = OptionType.SWITCH,
+			value_type = TYPE_BOOL,
+			value = false,
+		},
+		{
+			name = "show_heuristics",
+			option_type = OptionType.SWITCH,
+			value_type = TYPE_BOOL,
+			value = false,
+		},
+		{
+			name = "distance_scale",
+			option_type = OptionType.SPIN,
+			value_type = {
+				min = 1,
+				max = 100,
+				step = 1
+			},
+			value = 10,
+		}
+	],
+	graph = {
+		nodes = [],
+		edges = [],
+		next_id = 0,
 	}
-]
+}
 
 
 # Safe way to get option value
 func get_option(name : String) -> Variant:
-	for i in data:
+	for i in data.get("options", []):
 		if i.get("name", "") == name:
 			return i.get("value", null)
 	return null
 
 
 func _init() -> void:
-	graph = GraphData.new()
+	graph = GraphData.new(data.get("graph", {}))
 
 
 func get_graph() -> GraphData:
 	return graph
-
-
-
