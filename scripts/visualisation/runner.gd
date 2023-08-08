56c02e930cef5b8b7a7b2c0043ec06ac6555397b
Matthew Bannock <69541270+MattBann@users.noreply.github.com>
2023-08-08 16:37:19 +0100

    Add IDDFS algorithm

--- scripts/visualisation/runner.gd
+++ scripts/visualisation/runner.gd
@@ -27,16 +27,24 @@ var expansion_list : Array[VisualisationNodeData] = []
 var visited : Array[int] = []
 var current_node : VisualisationNodeData
 var completed_path : bool = false
+var algorithm_variables : Dictionary = {}
 
 
 # Initialise the Runner by appending the start node to the queue. If the start node does not exist, tells Controller to abort
 func _init() -> void:
 	var start := Controller.get_current_config().get_graph().get_start_node()
-	if not start:
+	if start == null:
 		Controller.abort_visualisation()
 		return
 	queue.append(VisualisationNodeData.new(start.id, 0, null))
 	current_stage = Stages.SELECT_NODE
+	# Setup special variables certain algorithms
+	if Controller.get_current_config().get_option("algorithm") == SetupData.Algorithm.ITERATIVE_DEEPENING_DEPTH_FIRST_SEARCH:
+		# Set max_depth var
+		algorithm_variables["max_depth"] = 0
+		# Set current_depth var
+		algorithm_variables["current_depth"] = -1
+		algorithm_variables["depth_stack"] = [0]
 
 
 # Execute the next stage of the algorithm from the steps in Stages
@@ -55,6 +63,9 @@ func step() -> void:
 				if current_node.node_id not in visited:
 					break
 			expansion_list.clear()
+			# If current_depth is defined, update its value
+			if algorithm_variables.has("current_depth"):
+				algorithm_variables["current_depth"] = (algorithm_variables.get("depth_stack", []) as Array).pop_front()
 			current_node_changed.emit(current_node)
 			queue_changed.emit(queue)
 		
@@ -80,18 +91,20 @@ func step() -> void:
 		
 		(Stages.EXPAND_OUT):
 			# Expand outwards from the current node, ignoring visited nodes
-			for edge in Controller.get_current_config().get_graph().get_edges_from(current_node.node_id):
-				if not edge.to in visited:
-					print(edge.weight)
-					expansion_list.append(VisualisationNodeData.new(edge.to, current_node.cost + (1.0 if Controller.get_current_config().get_option("force_uniform_path_cost") else edge.weight), current_node))
-			expansion_list_changed.emit(expansion_list)
+			# Special case: IDDFS: if at depth limit don't expand
+			if algorithm_variables.get("current_depth", -1) != algorithm_variables.get("max_depth", 0):
+				for edge in Controller.get_current_config().get_graph().get_edges_from(current_node.node_id):
+					if not edge.to in visited:
+						print(edge.weight)
+						expansion_list.append(VisualisationNodeData.new(edge.to, current_node.cost + (1.0 if Controller.get_current_config().get_option("force_uniform_path_cost") else edge.weight), current_node))
+				expansion_list_changed.emit(expansion_list)
 		
 		(Stages.ENQUEUE):
 			# Add the expanded nodes to the queue, using the chosen search type's enqueueing algorithm and mark node as visited
+			visited.append(current_node.node_id)
 			enqueue()
 			expansion_list.clear()
 			expansion_list_changed.emit(expansion_list)
-			visited.append(current_node.node_id)
 
 	# Output state in console for debugging
 	if OS.is_debug_build():
@@ -100,6 +113,7 @@ func step() -> void:
 		print("Expansion: ", expansion_list)
 		print("Queue: ", queue)
 		print("Visisted: ", visited)
+		print("Algorithm variables: ", algorithm_variables)
 		print("")
 
 	# Tell Controller that something has changed
@@ -132,4 +146,19 @@ func enqueue() -> void:
 			queue.append_array(expansion_list)
 			queue.sort_custom(func (a:VisualisationNodeData,b:VisualisationNodeData): 
 				return a.cost + Controller.get_current_config().get_graph().get_node(a.node_id).heuristic_value < b.cost + Controller.get_current_config().get_graph().get_node(b.node_id).heuristic_value)
+		
+		(SetupData.Algorithm.ITERATIVE_DEEPENING_DEPTH_FIRST_SEARCH):
+			# Enqueue at start, unless at the max depth for this iteration, in which case start over with max_depth+1
+			# (Expansion list should aready be empty if at max depth)
+			for i in expansion_list:
+				queue.push_front(i)
+				algorithm_variables.get("depth_stack", []).push_front(algorithm_variables.get("current_depth", 0)+1)
+			# If queue is empty, then reset depth first search with an increased max_depth
+			if queue.is_empty():
+				visited.clear()
+				queue.append(VisualisationNodeData.new(Controller.get_current_config().get_graph().get_start_node().id, 0, null))
+				algorithm_variables.get("depth_stack", []).push_front(0)
+				algorithm_variables["max_depth"] = algorithm_variables.get("max_depth", 0) + 1
+				algorithm_variables["current_depth"] += 1
+			
 	queue_changed.emit(queue)
